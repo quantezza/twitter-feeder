2fbd9e22ac1c932e278b3955e4fdfe42c4ab2e9e
Surendra Reddy <sureddy@quantiply.com>
2015-11-04 11:17:43 -0800

    Changed search string to #NationalStressAwarenessDay

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ ACCESS_TOKEN_KEY = ''
 ACCESS_TOKEN_SECRET = ''
 KAFKA_BROKER=''
 
-SEARCH_TERM = 'Hana'
+SEARCH_TERM = '#NationalStressAwarenessDay'
 
 twitter_metrics = {}
 twitter_metrics["tweets-consumed"] = 0
